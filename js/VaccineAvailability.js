7ca2b443aff346174df66bc78791e2dd4ddffa78
Utkarsh Yadav <37462832+utkarshyadav46@users.noreply.github.com>
2021-05-28 14:52:13 +0530

    update

--- js/VaccineAvailability.js
+++ js/VaccineAvailability.js
@@ -100,7 +100,7 @@ function addCellColor(tr, val,val_1) {
       document.getElementById("vaccine_slot").innerHTML="";
          document.getElementById("vaccine_slot").innerHTML += '<input type="text" id="myInput" onkeyup="searchFunction()" placeholder="Search for CHC names/pincode/vaccine name.." title="Type in a name">';
          document.getElementById("vaccine_slot").innerHTML += "<label style='float: right'></label><select id='ageGroup' onchange='showVaccine()' style='float: right'><option value='18' disabled selected>Select Age Group</option><option value='18'>18 - 45 years </option><option value='45'>45 years or above</option></select>";
-         document.getElementById("vaccine_slot").innerHTML += "<button onclick='getLocation()''> Locate Me</button>";
+        //  document.getElementById("vaccine_slot").innerHTML += "<button onclick='getLocation()''> Location</button>";
 
          var district = document.forms["vaccineForm"]["citySelect"].value;
           var today = new Date();
@@ -137,7 +137,7 @@ function addCellColor(tr, val,val_1) {
                        vaccinedata=JSON.parse(request.response);
                       console.log(vaccinedata.centers);
                       console.log(vaccinedata.centers[1].sessions['0'].vaccine);
-                      table.innerHTML +="<table id='tbl'class='table-responsive' style='text-align:center;'><tr><th> Center Name  </th><th>Age Group</th><th>"+dateFormat(new Date)+"</th><th>"+dateFormat(new Date(yyyy, mm-1, dd+1))+"</th><th>"+dateFormat(new Date(yyyy, mm-1, dd+2))+"</th><th>"+dateFormat( new Date(yyyy, mm-1, dd+3)) +"</th><th>"+dateFormat( new Date(yyyy, mm-1, dd+4))+"</th></tr>";
+                      table.innerHTML +="<table id='tbl'class='table table-borderless'><tr><th> Center Name  </th><th>Age Group</th><th>"+dateFormat(new Date)+"</th><th>"+dateFormat(new Date(yyyy, mm-1, dd+1))+"</th><th>"+dateFormat(new Date(yyyy, mm-1, dd+2))+"</th><th>"+dateFormat( new Date(yyyy, mm-1, dd+3)) +"</th><th>"+dateFormat( new Date(yyyy, mm-1, dd+4))+"</th></tr>";
                         console.log(vaccinedata.centers[1].sessions);
                       tbl1 = document.getElementById('tbl');
                       for (var i = 0; i <vaccinedata.centers.length; i++) 
